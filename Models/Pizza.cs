ccdfae05bf4e93b78dfac10dc21bdee744b68aa1
Dennis <denniskimtai1@gmail.com>
2023-08-17 17:03:30 +0300

    First commit

---
+++ Models/Pizza.cs
@@ -0,0 +1,13 @@
+﻿namespace PizzaApi.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Size { get; set; }
+        public string? Toppings { get; set; }
+
+        public bool isReady { get; set; }
+
+    }
+}
---
+++ Services/PizzaService.cs
@@ -0,0 +1,61 @@
+﻿using PizzaApi.Models;
+
+namespace PizzaApi.Services
+{
+    public class PizzaService
+    {
+        public static List<Pizza> Pizzas { get; } = new List<Pizza>(){
+                new Pizza() { Id = 1,
+                Name = "Classic Italian",
+                Size = "Small",
+                Toppings = "Pepperoni",
+                isReady = true}
+            };
+        public PizzaService()
+        {
+        
+        }
+
+        public static List<Pizza> GetAll() => Pizzas;
+
+        public static Pizza? Get(int id)
+        {
+            return Pizzas?.FirstOrDefault(p => p.Id == id);
+        }
+
+        public static void Add(Pizza pizza)
+        {
+            Pizzas?.Add(pizza);
+        }
+
+        public static void Remove(int id)
+        {
+            //check the id and get the pizza item using Get method
+            var pizza = Get(id);
+
+            //check if pizza item exist  in pizzas list
+            if(pizza != null)
+            {
+                Pizzas?.Remove(pizza);
+            }
+            else
+            {
+                return;
+            }
+            
+        }
+
+        public static void Update(Pizza pizza)
+        {
+            var index = Pizzas.FindIndex(p => p.Id == pizza.Id);
+
+            if(index == -1)
+            {
+                return;
+            }
+
+            Pizzas[index] = pizza;
+        }
+
+    }
+}
